dc434c332165323b7072de5a10adc348258f6267
SaulEspinoza7 <98200796+SaulEspinoza7@users.noreply.github.com>
2022-10-20 19:15:12 -0400

    Add files via upload

---
+++ demo/src/main/java/com/saul/app/demo/RestApIbookApplication.java
@@ -0,0 +1,13 @@
+package com.saul.app.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApIbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApIbookApplication.class, args);
+	}
+
+}
